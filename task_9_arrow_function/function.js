3344d62827491a64543fa77fc144880e04724d22
Vasil Laba <vasillaba1@gmail.com>
2023-07-24 18:11:26 +0200

    task 9 extended

--- task_9_arrow_function/function.js
+++ task_9_arrow_function/function.js
@@ -1,4 +1,8 @@
 let print = (text) => console.log(text);
+let sum = (a, b) => a + b;
+let sayHello = (name) => `Hello, ${name}`;
 
 print("hello");
 print("world");
+console.log(sum(5, 1));
+console.log(sayHello("Dima"));
