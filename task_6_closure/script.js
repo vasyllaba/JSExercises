b7b5837fee551ac01f3d60cc72359239382461c3
Vasil Laba <vasillaba1@gmail.com>
2023-07-24 14:58:15 +0200

    task 6 plus

--- task_6_closure/script.js
+++ task_6_closure/script.js
@@ -1,19 +1,25 @@
 {
     let n = 10;
-    let z = 1; //2 //3 //4
+    let l = 1; //2 //3 //4
     function outer(n) {
         let x = 1;
-        return function (y) { z++; return n + y + x + z}
+        return function (y) { l++; return n + y + x + l}
     }
 
-
+    function getL(){
+        return l;
+    }
 }
+console.log("L = " + getL())
 func = outer(2)
 console.log(func(1))
 console.log(func(2))
 console.log(func(2))
+console.log("L = " + getL())
 
 func2 = outer(2)
 console.log(func2(1))
 console.log(func2(1))
 console.log(func2(4))
+console.log("L = " + getL())
+
